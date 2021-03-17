003ad7c6b2a5b9f39568dc892cb8d78036a70690
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2021-03-17 15:29:48 +0100

    feat(annotations): Add annotations to rewrite requests

--- internal/caddy/ingress.go
+++ internal/caddy/ingress.go
@@ -6,25 +6,77 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp/reverseproxy"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp/rewrite"
 	"github.com/caddyserver/ingress/internal/controller"
 	"k8s.io/api/networking/v1beta1"
 )
 
+const (
+	annotationPrefix             = "caddy.ingress.kubernetes.io"
+	rewriteToAnnotation          = "rewrite-to"
+	rewriteStripPrefixAnnotation = "rewrite-strip-prefix"
+	disableSSLRedirect           = "disable-ssl-redirect"
+)
+
+func getAnnotation(ing *v1beta1.Ingress, rule string) string {
+	return ing.Annotations[annotationPrefix+"/"+rule]
+}
+
 // TODO :- configure log middleware for all routes
-func baseRoute(upstream string) caddyhttp.Route {
-	return caddyhttp.Route{
-		HandlersRaw: []json.RawMessage{
-			json.RawMessage(`
-			{
-				"handler": "reverse_proxy",
-				"upstreams": [
-						{
-								"dial": "` + fmt.Sprintf("%s", upstream) + `"
-						}
-				]
-			}
-		`),
+func generateRoute(ing *v1beta1.Ingress, rule v1beta1.IngressRule, path v1beta1.HTTPIngressPath) caddyhttp.Route {
+	var handlers []json.RawMessage
+
+	// Generate handlers
+	rewriteTo := getAnnotation(ing, rewriteToAnnotation)
+	if rewriteTo != "" {
+		handlers = append(handlers, caddyconfig.JSONModuleObject(
+			rewrite.Rewrite{URI: rewriteTo},
+			"handler", "rewrite", nil,
+		))
+	}
+
+	rewriteStripPrefix := getAnnotation(ing, rewriteStripPrefixAnnotation)
+	if rewriteStripPrefix != "" {
+		handlers = append(handlers, caddyconfig.JSONModuleObject(
+			rewrite.Rewrite{StripPathPrefix: rewriteStripPrefix},
+			"handler", "rewrite", nil,
+		))
+	}
+
+	clusterHostName := fmt.Sprintf("%v.%v.svc.cluster.local:%d", path.Backend.ServiceName, ing.Namespace, path.Backend.ServicePort.IntVal)
+	handlers = append(handlers, caddyconfig.JSONModuleObject(
+		reverseproxy.Handler{
+			Upstreams: reverseproxy.UpstreamPool{
+				{Dial: clusterHostName},
+			},
 		},
+		"handler", "reverse_proxy", nil,
+	))
+
+	// Generate matchers
+	match := caddy.ModuleMap{}
+
+	if getAnnotation(ing, disableSSLRedirect) != "true" {
+		match["protocol"] = caddyconfig.JSON(caddyhttp.MatchProtocol("https"), nil)
+	}
+
+	if rule.Host != "" {
+		match["host"] = caddyconfig.JSON(caddyhttp.MatchHost{rule.Host}, nil)
+	}
+
+	if path.Path != "" {
+		p := path.Path
+
+		if *path.PathType == v1beta1.PathTypePrefix {
+			p += "*"
+		}
+		match["path"] = caddyconfig.JSON(caddyhttp.MatchPath{p}, nil)
+	}
+
+	return caddyhttp.Route{
+		HandlersRaw:    handlers,
+		MatcherSetsRaw: []caddy.ModuleMap{match},
 	}
 }
 
@@ -40,29 +92,7 @@ func LoadIngressConfig(config *Config, store *controller.Store) error {
 	for _, ing := range store.Ingresses {
 		for _, rule := range ing.Spec.Rules {
 			for _, path := range rule.HTTP.Paths {
-				clusterHostName := fmt.Sprintf("%v.%v.svc.cluster.local:%d", path.Backend.ServiceName, ing.Namespace, path.Backend.ServicePort.IntVal)
-				r := baseRoute(clusterHostName)
-
-				match := caddy.ModuleMap{
-					// match only on https protocol to allow HTTPS redirects
-					// TODO Let user disable this to serve HTTP requests
-					"protocol": caddyconfig.JSON(caddyhttp.MatchProtocol("https"), nil),
-				}
-
-				if rule.Host != "" {
-					match["host"] = caddyconfig.JSON(caddyhttp.MatchHost{rule.Host}, nil)
-				}
-
-				if path.Path != "" {
-					p := path.Path
-
-					if *path.PathType == v1beta1.PathTypePrefix {
-						p += "*"
-					}
-					match["path"] = caddyconfig.JSON(caddyhttp.MatchPath{p}, nil)
-				}
-
-				r.MatcherSetsRaw = []caddy.ModuleMap{match}
+				r := generateRoute(ing, rule, path)
 
 				routes = append(routes, r)
 			}
