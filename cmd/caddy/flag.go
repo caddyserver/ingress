506bd98c48ffb08f51cf241c4533746c849cae19
Michael Teuscher <michael.teuscher@pm.me>
2022-08-19 23:30:41 +0200

    Make use of the new options

--- cmd/caddy/flag.go
+++ cmd/caddy/flag.go
@@ -11,14 +11,14 @@ func parseFlags() store.Options {
 	var namespace string
 	flag.StringVar(&namespace, "namespace", "", "the namespace that you would like to observe kubernetes ingress resources in.")
 
-	var configMapName string
-	flag.StringVar(&configMapName, "config-map", "", "defines the config map name from where to load global options")
-
 	var className string
 	flag.StringVar(&className, "class-name", "caddy", "class name of the ingress controller")
 
 	var classNameRequired bool
 	flag.BoolVar(&classNameRequired, "class-name-required", false, "only allow ingress resources with a matching ingress class name")
+
+	var configMapName string
+	flag.StringVar(&configMapName, "config-map", "", "defines the config map name from where to load global options")
 
 	var leaseId string
 	flag.StringVar(&leaseId, "lease-id", "", "defines the id of this instance for certmagic lock")
@@ -32,10 +32,12 @@ func parseFlags() store.Options {
 	flag.Parse()
 
 	return store.Options{
-		WatchNamespace: namespace,
-		ConfigMapName:  configMapName,
-		Verbose:        verbose,
-		LeaseId:        leaseId,
-		PluginsOrder:   strings.Split(pluginsOrder, ","),
+		WatchNamespace:    namespace,
+		ClassName:         className,
+		ClassNameRequired: classNameRequired,
+		ConfigMapName:     configMapName,
+		Verbose:           verbose,
+		LeaseId:           leaseId,
+		PluginsOrder:      strings.Split(pluginsOrder, ","),
 	}
 }
