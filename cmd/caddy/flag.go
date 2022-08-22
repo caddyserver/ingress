f7739a6837d65779d242eea865108e2950d3f550
Nila <43315617+nilathedragon@users.noreply.github.com>
2022-08-22 11:50:00 +0200

    Add config options for class name and class name required (#99)

--- cmd/caddy/flag.go
+++ cmd/caddy/flag.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"flag"
-	"github.com/caddyserver/ingress/pkg/store"
 	"strings"
+
+	"github.com/caddyserver/ingress/pkg/store"
 )
 
 func parseFlags() store.Options {
 	var namespace string
 	flag.StringVar(&namespace, "namespace", "", "the namespace that you would like to observe kubernetes ingress resources in.")
+
+	var className string
+	flag.StringVar(&className, "class-name", "caddy", "class name of the ingress controller")
+
+	var classNameRequired bool
+	flag.BoolVar(&classNameRequired, "class-name-required", false, "only allow ingress resources with a matching ingress class name")
 
 	var configMapName string
 	flag.StringVar(&configMapName, "config-map", "", "defines the config map name from where to load global options")
@@ -25,10 +32,12 @@ func parseFlags() store.Options {
 	flag.Parse()
 
 	return store.Options{
-		WatchNamespace: namespace,
-		ConfigMapName:  configMapName,
-		Verbose:        verbose,
-		LeaseId:        leaseId,
-		PluginsOrder:   strings.Split(pluginsOrder, ","),
+		WatchNamespace:    namespace,
+		ClassName:         className,
+		ClassNameRequired: classNameRequired,
+		ConfigMapName:     configMapName,
+		Verbose:           verbose,
+		LeaseId:           leaseId,
+		PluginsOrder:      strings.Split(pluginsOrder, ","),
 	}
 }
